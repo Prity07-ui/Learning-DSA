0a36096ab9a734e5f7fd5e62ba7e1c8747b854d2
Prity <pk3183121@gmail.com>
2026-04-10 23:44:21 +0530

    Add files via upload

---
+++ A of Squar.cpp
@@ -0,0 +1,14 @@
+// WAP to print Area of square
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a, area;
+	cout<<"Enter the value of a :";
+	cin>>a;
+	
+	area=a*a;
+	cout<<area;
+	return 0;
+}
