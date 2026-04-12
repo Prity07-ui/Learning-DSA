c1982a4315ac7970a8d1edec65a9ce1cf9b87686
Prity <pk3183121@gmail.com>
2026-04-12 11:06:19 +0530

    Add files via upload

---
+++ ForLoop.cpp
@@ -0,0 +1,17 @@
+// For loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n =3;
+	int sum=0;
+	
+    for(int i=1; i<=n; i++){
+	sum +=1;
+ }
+ 
+	cout<<"Sum ="<<sum <<endl;
+return 0;
+
+}
