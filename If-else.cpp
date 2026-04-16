1fd4545a33142e664fe958679e3d477acc2eb29e
Prity <pk3183121@gmail.com>
2026-04-16 11:39:22 +0530

    Add files via upload

---
+++ If-else.cpp
@@ -0,0 +1,22 @@
+// if-else
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int age;
+	
+	cout<<"Enter the age :";
+	cin>>age;
+	
+	if (age>=18){
+		cout<<" Your can vote \n";
+			
+	}
+		else{
+			
+			cout<<"Your can not vote \n";
+		}
+	return 0;
+}
