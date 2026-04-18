c95a422264927186565dad0961935c5c64ca6ac7
Prity <pk3183121@gmail.com>
2026-04-18 18:39:48 +0530

    Add files via upload

---
+++ Typecast1.cpp
@@ -0,0 +1,10 @@
+// Typecasting (1.Conversion(Implicit))
+
+#include <iostream>
+using namespace std;
+int main(){
+	char grade ='A';
+	int value = grade;
+	cout<< value << endl;
+	return 0; // 65
+}
