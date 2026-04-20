d0539e3c27fe43ee80e4cae4a85044dcdd3f7789
Prity <pk3183121@gmail.com>
2026-04-20 21:44:34 +0530

    Add files via upload

---
+++ Upper & Lowercase.cpp
@@ -0,0 +1,18 @@
+// Uppercase or Lowercase
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char ch;
+	cout<<"Enter char :";
+	cin>>ch;
+	
+	if(ch>=65 && ch<=90){
+		cout<<"Uppercase";
+	}
+	else
+	cout<<"Lowercase";
+	
+	return 0;
+}
